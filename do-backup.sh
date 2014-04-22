e1ec8c5cc81ebe92dda196d6c9c368e0ce80ee32
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-04-22 19:45:47 +0900

    say error when command crontab is not found in the $PATH

--- do-backup.sh
+++ do-backup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 PATH=/usr/bin:/bin:$PATH
 
+if ! which crontab >/dev/null; then
+	echo crontab: command not found >&2
+	exit 1
+fi
+
 crontab -l >crontab.$USER
 git add crontab.$USER
 git commit crontab.$USER -m "update for user $USER" >/dev/null || true
